16117ab01d6a4aa319eec80c0691323ac6dc6c5e
Mario <mromanre@asu.edu>
2019-10-07 15:21:08 -0700

    Initial Version

---
+++ Assignment2 CSE360/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,42 @@
+/* Mario Roman Reyes
+ * CSE 360-70641
+ * Assignment 2 
+ * Class called AddingMachine that adds and subtracts an int from the private int total 
+ */
+package cse360assign2;
+
+public class AddingMachine 
+{
+
+	private int total;//Creates private int variable
+	
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal ()
+	{
+		return 0;
+	}
+	
+	public void add (int value) 
+	{
+		
+	}
+	
+	public void subtract (int value)
+	{
+		
+	}
+		
+	public String toString ()
+	{
+		return "";
+	}
+
+	public void clear()
+	{
+	
+	}
+}
