b875a4b0261cb14488cd52f14acdd88b861bdc98
Mario <mromanre@asu.edu>
2019-10-07 16:41:32 -0700

    Second Version

--- Assignment2 CSE360/src/cse360assign2/AddingMachine.java
+++ Assignment2 CSE360/src/cse360assign2/AddingMachine.java
@@ -1,7 +1,8 @@
 /* Mario Roman Reyes
  * CSE 360-70641
  * Assignment 2 
- * Class called AddingMachine that adds and subtracts an int from the private int total 
+ * Class called AddingMachine that adds and subtracts an int from the private int total.
+ * Class also stores the transaction history in a string and clears the total and transaction history.
  */
 package cse360assign2;
 
@@ -11,34 +12,34 @@ public class AddingMachine
 	private int total;//Creates private int variable for total
 	private String history = "0";//Creates private string variable for history
 	
-	public AddingMachine () 
+	public AddingMachine () //Constructor
 	{
 		total = 0;
 	}
 	
-	public int getTotal()
+	public int getTotal()// returns the private int variable total 
 	{
 		return total;
 	}
 	
-	public void add(int value) 
+	public void add(int value)//Function to add a value to the total
 	{
-		total += value;
-		history = history + " + " + Integer.toString(value);
+		total += value;//adds the value to the total
+		history = history + " + " + Integer.toString(value);//changes string history to keep track of transaction history
 	}
 	
-	public void subtract(int value)
+	public void subtract(int value)//Function to subtract a value from the total
 	{
-		total -= value;
-		history = history + " - " + Integer.toString(value);
+		total -= value;//subtracts the value from the total
+		history = history + " - " + Integer.toString(value);//changes string history to keep track of transaction history
 	}
 		
-	public String toString()
+	public String toString()//returns string history
 	{
 		return history;
 	}
 
-	public void clear()
+	public void clear()//clears the total
 	{
 		total = 0;
 		history = "0";
